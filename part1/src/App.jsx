e33bf008bd859b84aa97ba329bf0d37b9bc1262e
Ad0t <171239099+Ad0t@users.noreply.github.com>
2025-06-14 13:15:02 +0530

    practice-a.4

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,21 +1,25 @@
 import './App.css'
 import { createElement } from 'react'
 
-// Practice 3
-const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  return createElement(
-    'div',
-    null,
-    createElement(
-      'p', null, 'Hello world, it is ', now.toString()
-    ),
-    createElement(
-      'p', null, a, ' plus ', b, ' is ', a + b
-    )
+// Practice 4
+const Hello = () => {
+  return (
+    <div>
+      <p>Hello duniya!</p>
+    </div>
   )
 }
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello />
+      <Hello />
+      <Hello />
+    </div>
+  )
+}
+
 export default App
   
