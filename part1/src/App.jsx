a12bed4da278d1c2c0c8cdf3e47a1924564d2906
Ad0t <171239099+Ad0t@users.noreply.github.com>
2025-06-14 14:01:48 +0530

    exercise-1.1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,38 +1,32 @@
 import './App.css'
 
-// Practice 4
-const Hello = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <p>I am {props.name} {props.sname} of {props.age} years</p>
-    </div>
-  )
-}
-
-const Footer = () => {
-  return (
-    <div>
-      greeting app created by <a href='https://github.com/Ad0t'>Ad0t</a>
-    </div>
-  )
-}
-
 const App = () => {
-  const friends = [
-    { name: 'Peter', age: 4 },
-    { name: 'Maya', age: 10 },
-  ]
-  const friends2 = [ 'Peter', 'Man']
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
   return (
     <div>
-      <p>{friends[0].name} {friends[0].age}</p>
-      <p>{friends[1].name} {friends[1].age}</p>
-      <p>{friends2}</p>
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
 }
 
 export default App
+
+
   
